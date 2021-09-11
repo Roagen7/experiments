0d390c0b8e754f44cf9b5baa122ee80957d7cc85
Roagen7 <harek2388@gmail.com>
2021-09-11 23:55:41 +0200

    perlin noise GPU

--- projects/diffusion/fs.glsl
+++ projects/diffusion/fs.glsl
@@ -4,16 +4,27 @@ in vec3 Color;
 
 out vec4 FragColor;
 
-uniform float scale;
-#define AREA 32
+uniform int AREA;
 
 
-uniform float vecx[AREA];
-uniform float vecy[AREA];
-
-float rand(vec2 co){
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+float interpolate(float a0, float a1, float w){
+    return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
 }
+
+vec2 randomGradient(int ix, int iy) {
+    // No precomputed gradients mean this works for any number of grid coordinates
+    int w = 8;
+    int s = w / 2; // rotation width
+    int a = ix, b = iy;
+    a = a * 3284157443; b ^= a << s | a >> w-s;
+    b *= 1911520717; a ^= b << s | b >> w-s;
+    a *= 2048419325;
+    float random = a * (3.14159265 / (4 >> 1)); // in [0, 2*Pi]
+    vec2 v;
+    v.x = sin(random); v.y = cos(random);
+    return v;
+}
+
 
 float dot(vec2 v1, vec2 v2){
     return v1.x * v2.x + v1.y * v2.y;
@@ -22,37 +33,47 @@ float dot(vec2 v1, vec2 v2){
 float dotGradient(float ix, float iy, float x, float y){
     float dx = x - ix;
     float dy = y - iy;
-    return dot(vec2(dx,dy), vec2(vecx[int(ix)],vecy[int(iy)]));
+
+    vec2 gradient = randomGradient(int(ix),int(iy));
+    return gradient.x * dx + gradient.y * dy;
+//    return dot(vec2(dx,dy), vec2(vecx[int(ix)],vecy[int(iy)]));
 }
 
 float noise(float x, float y){
-    float x_AREA =  x/ 1920.0 * AREA;
-    float y_AREA = y / 1080.0 * AREA;
+    x/=AREA;
+    y/=AREA;
 
-    float x0 = floor(x_AREA);
-    float y0 = floor(y_AREA);
-    float x1 = x0 + 1;
-    float y1 = y0 + 1;
+    int x0 = int(x);
+    int x1 = x0 + 1;
+    int y0 = int(y);
+    int y1 = y0 + 1;
 
-    float sx = x_AREA - float(x0);
-    float sy = y_AREA - float(y0);
+    float sx = x - float(x0);
+    float sy = y - float(y0);
 
-    float n0 = dotGradient(x0,y0, x_AREA,y_AREA);
-    float n1 = dotGradient(x1,y0,x_AREA,y_AREA);
-    float ix0 = mix(n0,n1,sx);
-    n0 = dotGradient(x0,y1,x_AREA,y_AREA);
-    n1 = dotGradient(x1,y1,x_AREA,y_AREA);
-    float ix1 = mix(n0,n1,sx);
+    float n0, n1, ix0, ix1, value;
 
-    return  mix(ix0,ix1, sy);
+    n0 = dotGradient(x0, y0, x, y);
+    n1 = dotGradient(x1, y0, x, y);
+    ix0 = interpolate(n0, n1, sx);
+
+    n0 = dotGradient(x0, y1, x, y);
+    n1 = dotGradient(x1, y1, x, y);
+    ix1 = interpolate(n0, n1, sx);
+
+    value = interpolate(ix0, ix1, sy);
+
+    value = max(min(1.0f,value),-1.0f);
+    return (value + 1.0)/ 2.0;
+//    return std::max(0.0f,std::min(1.0f, val));
 }
 
 void main() {
 
 
-//    float col = noise(Position.x, Position.y);
+    float col = noise(Position.x,Position.y);
 
-//    FragColor = vec4(col,col,col, 1.0);
-    FragColor = vec4(Color,1.0);
+    FragColor = vec4(col,col,col, 1.0);
+
 
 }
--- projects/diffusion/main.cpp
+++ projects/diffusion/main.cpp
@@ -11,87 +11,12 @@ const int width = 1920;
 const int height = 1080;
 
 const int AREA = 32;
-float vecx[AREA];
-float vecy[AREA];
-
-glm::vec2 randomGradient(int ix, int iy) {
-    // No precomputed gradients mean this works for any number of grid coordinates
-    const unsigned w = 8 * sizeof(unsigned);
-    const unsigned s = w / 2; // rotation width
-    unsigned a = ix, b = iy;
-    a *= 3284157443; b ^= a << s | a >> w-s;
-    b *= 1911520717; a ^= b << s | b >> w-s;
-    a *= 2048419325;
-    float random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
-    glm::vec2 v;
-    v.x = sin(random); v.y = cos(random);
-    return v;
-}
-
-
-float interpolate(float a0, float a1, float w){
-    return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
-}
-
-
-float dot(glm::vec2 v1, glm::vec2 v2){
-    return v1.x * v2.x + v1.y * v2.y;
-}
-
-float dotGradient(int ix, int iy, float x, float y){
-    float dx = x - (float)ix;
-    float dy = y - (float)iy;
-    glm::vec2 gradient = randomGradient(ix,iy);
-//    glm::vec2 gradient =  glm::vec2(vecx[int(ix)],vecy[int(iy)]);
-//    return dot(glm::vec2(dx,dy), glm::vec2(vecx[int(ix)],vecy[int(iy)]));
-    return (dx*gradient.x + dy*gradient.y);
-}
-
-float noise(float x, float y){
-    x/=AREA;
-    y/=AREA;
-//    x = x * AREA / 1920;
-//    y = y * AREA/ 1920;
-    int x0 = (int)x;
-    int x1 = x0 + 1;
-    int y0 = (int)y;
-    int y1 = y0 + 1;
-
-
-    float sx = x - (float)x0;
-    float sy = y - (float)y0;
-
-    float n0, n1, ix0, ix1, value;
-
-    n0 = dotGradient(x0, y0, x, y);
-    n1 = dotGradient(x1, y0, x, y);
-    ix0 = interpolate(n0, n1, sx);
-
-    n0 = dotGradient(x0, y1, x, y);
-    n1 = dotGradient(x1, y1, x, y);
-    ix1 = interpolate(n0, n1, sx);
-
-    value = interpolate(ix0, ix1, sy);
-
-    value = std::max(std::min(1.0f,value),-1.0f);
-    return (value + 1.0)/ 2.0;
-//    return std::max(0.0f,std::min(1.0f, val));
-}
-
 
 
 void diffusion::gl_main() {
 
     GLFWwindow* window;
     createWindow(window,width,height);
-    srand(time(NULL));
-
-    for(int i =0; i < AREA; i++){
-        vecx[i] = (float) rand()/RAND_MAX;
-        vecy[i] = (float) rand()/RAND_MAX;
-
-    }
-
 
 
     std::vector<GLfloat> points;
@@ -103,11 +28,10 @@ void diffusion::gl_main() {
             points.push_back(x);
             points.push_back(y);
 
-            float v = noise(x,y);
 
-            points.push_back(v);
-            points.push_back(v);
-            points.push_back(v);
+            points.push_back(1.0f);
+            points.push_back(1.0f);
+            points.push_back(1.0f);
         }
     }
 
@@ -120,8 +44,7 @@ void diffusion::gl_main() {
     glUseProgram(shaderProgram);
     glUniform1i(glGetUniformLocation(shaderProgram,"WIDTH"),width);
     glUniform1i(glGetUniformLocation(shaderProgram,"HEIGHT"),height);
-    glUniform1fv(glGetUniformLocation(shaderProgram, "vecx"), AREA, vecx);
-    glUniform1fv(glGetUniformLocation(shaderProgram,"vecy"),AREA,vecy);
+    glUniform1i(glGetUniformLocation(shaderProgram,"AREA"),AREA);
 
     GLuint VAO;
     bindData(points, VAO);
